a3f214f64826c7055193d3d77bd6f889391a9b5d
shahin759 cassime <230107249@aston.ac.uk>
2025-01-30 19:15:12 +0000

    updated func

--- lab2-functionalprimer/haskell/func_as_arg.hs
+++ lab2-functionalprimer/haskell/func_as_arg.hs
@@ -1,9 +1,13 @@
---create inpFunc
-inpFunc = [1..5] 
+-- Define a function to generate a list of integers from a to b
+inpFunc a b = [a..b]
 
---Define applicatorFunc
-applicatorFunc inpFunc s = if s=='s' then sum inpFunc else (sum inpFunc)/5  
+applicatorFunc inpFunc s
+    | s == 's'  = sum inpFunc             
+    | otherwise = (sum inpFunc) / fromIntegral (length inpFunc)
 
 main = do
-    let result = applicatorFunc inpFunc 'a' --Call applicatorFunc with inpFunc and 'a' as args
-    putStrLn("sum = " ++ show(result))
+  
+    let a = 1
+    let b = 5
+    let result = applicatorFunc (inpFunc a b) 's' -- Call applicatorFunc with inpFunc and 'a' as args
+    putStrLn("Result = " ++ show(result))
